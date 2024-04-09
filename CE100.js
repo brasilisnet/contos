25f871c05407de2d0bcd356e457574a16f464c71
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2024-04-09 17:15:51 +0900

    Add files via upload

---
+++ CE100.js
@@ -0,0 +1,10 @@
+//Javascript Created by Computerhope https://www.computerhope.com/
+//store the quotations in arrays
+var images = [],
+index = 0;
+images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img class='top' border='0' src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgDQNbw2rsJJP6tdS944_FmTi3k1oeGPfPR06dRQwgI15JQu4uBmuN6PmFRzfTn6fqrI3V6MmCfCln-WhiFwlAROCdZZql-mv16qj2CRiWH-Bp4wpON8J_HU-5oump8KGF9uMCT9nVwry53FXWFaQptukofuRAoJA1MAAMyTg_gs_e-UY4sVIBP22CgZj1C/s1024/contos-eroticos-302.webp' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img class='top' border='0' src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhoRsyW-TEle-iYjeEYitJqw6yDVeVcSh9sSpEIgFrFkHrhvyy6gp1EM5U72HmrgJYvz9T9HNzsB0r4ELqAkBG7RK1OFUzUu4Jvbw7nz_TohrdNf-F0Nxi0DmMMebG8LkPXfVc3G9j2C6c_2G4XAq3Cdhyphenhyphen1R91Hb1_s23ja-wfAebLYUo0sQ6AqfjVSZ20V/s1024/contos-eroticos-306.webp' width='100%' height='auto'></a>";
+images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img class='top' border='0' src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjF1SkDPskelbvJ7bayawxtC1nP8_VGKmFMLfh_yPbQuT9bqH4qepUkDrmnz6Fk33BeRzyTgRWVogUdPeLOs-r_JgwROWDY6fPLrhJqImZpvinVWL7Hix4lGyiOc9c4y9l5nWx3Rnz-zklitkXCft-0uf8YAoqAColH8xHcZFX8QlcFUvPaYEn1KFW_9VIQ/s1024/contos-eroticos-305.webp' width='100%' height='auto'></a>";
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
